98577c5682d52c190de418748c012d1560fe090c
Ryaanajambeg <ryaan.ajambeg@students.plymouth.ac.uk>
2021-05-25 16:33:24 +0100

    health nar added

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ document.querySelector('.scroll-btn').addEventListener
     ='unset';
     },1000)
 })
+class ProgressBar{
+    constructor(element, initialValue = 0){
+        this.valueElem = element.querySelector('.progress-bar-value');
+        this.fillElem = element.querySelector('.progress-bar-fill');
+
+        this.setValue(initialValue);
+
+        console.log(this.valueElem);
+        console.log(this.fillElem);
+    }
+
+    setValue (newValue){
+        if(newValue < 0){
+            newValue = 0;
+        }
+        if(newValue > 100){
+            newValue = 100;
+        }
+        this.value=newValue;
+        this.update();
+    }
+
+    update (){
+        const percentage= this.value + '%';
+
+        this.fillElem.style.width = percentage;
+        this.valueElem.textContent = percentage;
+    }
+}
+const pb1=new ProgressBar(document.querySelector('.progress-bar'), 78);

487bdf147ce928903c8c7f9542b3848f98dbe849
Ryaanajambeg <ryaan.ajambeg@students.plymouth.ac.uk>
2021-05-25 18:16:34 +0100

    chart added

--- script.js
+++ script.js
@@ -41,3 +41,41 @@ class ProgressBar{
     }
 }
 const pb1=new ProgressBar(document.querySelector('.progress-bar'), 78);
+
+let chart = document.getElementById('chart').getContext('2d');
+
+
+
+let connectchart = new Chart(chart, {
+        type: 'bar',
+        data:{
+            labels: ['01/07/2006', '02/07/2006', '03/07/2006', '04/07/2006', '05/07/2006'],
+            datasets: [{
+                label: 'Connections',
+                data: [
+                    5058,
+                    7437,
+                    7248,
+                    8280,
+                    27189
+                ],
+                backgroundColor:[
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(255, 159, 64, 0.6)',
+                ],
+                borderWidth:2,
+                borderColor: '#777',
+                hoverBorderWidth: 3,
+                hoverBorderColor:'#000',
+            }]
+        },
+        options: {}
+            
+      
+        
+
+
+});
